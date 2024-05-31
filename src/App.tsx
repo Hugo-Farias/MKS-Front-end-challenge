40235ca642aff21c613abb657421e0bb676bf6f5
Hugo-Farias <hg80rod@gmail.com>
2024-05-31 10:25:32 -0300

    header start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.scss";
+import Header from "./components/Header";
 
 function App() {
-  return null;
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const header = function () {
+  return (
+    <div className="header">
+      <h1>asdfsdf</h1>
+    </div>
+  );
+};
+
+export default header;
