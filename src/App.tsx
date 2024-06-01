180f06099eb2482b3329c6e90ffa3e46c22f1fc8
Hugo-Farias <hg80rod@gmail.com>
2024-06-01 08:39:57 -0300

    checkout start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Products from "./components/Products";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <>
       <Header />
       <Products />
+      <Checkout />
       <footer>MKS Sistemas © Todos os direitos reservados</footer>
     </>
   );
---
+++ src/components/Checkout.tsx
@@ -0,0 +1,11 @@
+import "./Checkout.scss";
+
+const Checkout = function () {
+  return (
+    <div className="cart-section">
+      <h1>Carrinho de compras</h1>
+    </div>
+  );
+};
+
+export default Checkout;
