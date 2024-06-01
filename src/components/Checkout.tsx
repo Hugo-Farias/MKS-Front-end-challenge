095b9d904942e94680b4724cec9f74dc968f283b
Hugo-Farias <hg80rod@gmail.com>
2024-06-01 08:53:33 -0300

    checkout top section ongoing

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,12 +1,19 @@
 import "./Checkout.scss";
+import closeBtn from "../assets/closeBtn.svg";
+// import x from "../assets/x.svg";
 
 const Checkout = function () {
   return (
     <div className="cart-section">
-      <h1>
-        Carrinho
-        <br /> de compras
-      </h1>
+      <div className="top">
+        <h1>
+          Carrinho
+          <br /> de compras
+        </h1>
+        <button className="close-btn">
+          <img src={closeBtn} alt="close button" />
+        </button>
+      </div>
     </div>
   );
 };
