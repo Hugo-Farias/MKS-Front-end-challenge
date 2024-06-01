66235f497cfb63a991e6c67e88fd0711c54cfca4
Hugo-Farias <hg80rod@gmail.com>
2024-05-31 23:37:49 -0300

    removed decimals from prices if zero

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,18 @@ type propsT = {
 
 const Card = function (props: propsT) {
   const { name, photo, price } = props;
+
+  // Remove decimals if zero
+  const newPrice = Number(price)
+    .toFixed(2)
+    .replace(/(\.0+|0+)$/, "");
+
   return (
     <div className="card">
       <img src={photo} alt={`${name} product photo`} />
       <div className="name-price-cont">
         <div className="name">{name}</div>
-        <div className="price">{price}</div>
+        <div className="price">{newPrice}</div>
       </div>
     </div>
   );
