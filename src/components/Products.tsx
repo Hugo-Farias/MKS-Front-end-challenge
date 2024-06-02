e405d8250047f345e0f4046612d4544879797497
Hugo-Farias <hg80rod@gmail.com>
2024-06-02 01:52:02 -0300

    api fetch + loading handling

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,12 +1,30 @@
 import "./Products.scss";
 import Card from "./Card";
-import { ProductsT } from "../types";
-import DUMMY_DATA from "../../DUMMY_DATA.json";
+import { ProductT } from "../types";
+import { useEffect, useState } from "react";
 
-const { products }: ProductsT = DUMMY_DATA;
+const API_URL =
+  "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=20&sortBy=name&orderBy=DESC";
 
 const Products = function () {
-  const jsx = products.map((v) => {
+  const [apiData, setApiData] = useState<ProductT[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setApiData(data.products);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData().then();
+  }, []);
+
+  if (!apiData) return <span className="loading" />;
+
+  const jsx = apiData.map((v) => {
     return <Card key={v.id} product={v} />;
   });
 
