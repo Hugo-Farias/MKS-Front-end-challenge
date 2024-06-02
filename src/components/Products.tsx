f8520534f70d9071eccdb162591edc7168f5dd6c
Hugo-Farias <hg80rod@gmail.com>
2024-06-02 10:17:46 -0300

    react-query implemented

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
 import "./Products.scss";
 import Card from "./Card";
-import { ProductT, ProductsT } from "../types";
-import { useEffect, useState } from "react";
+import { ProductsT, ProductT } from "../types";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import DUMMY_DATA from "../../DUMMY_DATA.json";
 
 const { products: dummy }: ProductsT = DUMMY_DATA;
@@ -9,26 +9,25 @@ const { products: dummy }: ProductsT = DUMMY_DATA;
 const API_URL =
   "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=name&orderBy=DESC";
 
+const queryFetch = async function (): Promise<ProductsT> {
+  const res = await fetch(API_URL);
+  if (!res.ok) throw new Error("Network response was not ok");
+  return await res.json();
+};
+
 const Products = function () {
-  const [apiData, setApiData] = useState<ProductT[] | null>(null);
+  const { data, isLoading }: UseQueryResult<ProductsT, boolean> = useQuery<
+    ProductsT,
+    boolean
+  >({
+    queryKey: ["products"],
+    queryFn: queryFetch,
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        setApiData(data.products);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchData().then();
-  }, []);
-
-  const loadedData = apiData || dummy;
+  const loadedData = data ? data.products : dummy;
 
   const jsx = loadedData.map((v: ProductT) => {
-    return <Card key={v.id} product={v} loading={apiData === null} />;
+    return <Card key={v.id} product={v} loading={isLoading} />;
   });
 
   return <div className="products">{jsx}</div>;
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const qClient = new QueryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={qClient}>
+      <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </Provider>
